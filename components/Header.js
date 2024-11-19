99704a3f8f19968c69c24a78c2e059c41e0c5dff
Zerxa Aeon Mamac <43023083+zerxzee@users.noreply.github.com>
2024-11-19 10:36:05 +0100

    Handle logging out

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import { signOut } from 'next-auth/react';
+import Modal from "./Modal";
+
 export default function Header() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleOpenModal = () => setIsModalVisible(true);
+    const handleCloseModal = () => setIsModalVisible(false);
+    const handleConfirm = () => {
+        handleSignOut();
+        handleCloseModal();
+    };
+
+    const handleSignOut = async () => {
+        await signOut({
+            redirect: true,
+            callbackUrl: '/',
+        });
+    };
+
     return (
         <header className="bg-neutral-300">
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
@@ -23,11 +43,23 @@ export default function Header() {
                         />
                     </a>
                 </div>
-                <div className="flex justify-end">
+                <div className="flex justify-end" onClick={handleOpenModal}>
                     <a href="#" className="text-sm/6 font-semibold text-gray-900">
                         Log out
                     </a>
                 </div>
+
+                {/* Logout modal */}
+                <Modal
+                    isVisible={isModalVisible}
+                    title="Logout"
+                    subtitle="Are you sure you want to logout?"
+                    confirmLabel="Logout"
+                    denyLabel="Cancel"
+                    confirmLabelBgColor="bg-red-500"
+                    onConfirm={handleConfirm}
+                    onDeny={handleCloseModal}
+                />
             </nav>
         </header>
     )
